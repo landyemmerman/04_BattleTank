cb6b90bb48cdc866f2a528057b3e1f6758fb4fcd
Landy Emmerman <landyemmerman@gmail.com>
2019-07-21 18:19:01 -0500

    BT32 Add Tick() to PlayerController

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "TankPlayerController.h"
 
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay(); // see 27. Using virtual and override.mp4 0:08:16
@@ -11,13 +12,32 @@ void ATankPlayerController::BeginPlay()
 
 	if (!ControlledTank) {
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing "));
-	}else{
+	}
+	else {
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
 	}
 }
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+
+}
+
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get world location ifline trace through crosshair]
+	// If it hits the landscape
+		// Tell controll tank to aim at this point.
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -21,4 +21,13 @@ public:
 	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	// Start the tank moving the barrel so that a shot would hit where 
+	//the crosshair intersects the world
+	void AimTowardsCrosshair();
 };
